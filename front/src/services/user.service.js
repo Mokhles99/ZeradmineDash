fc58c45de446d491cd3fbefa57e1e3938759f8e8
Mokhles <mokles.benali900@outlook.com>
2024-08-09 21:45:29 +0200

    IPA

--- front/src/services/user.service.js
+++ front/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "https://admin.szq.tn/api/api/test/";
+const API_URL = "https://admin.szq.tn/api/test/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");
